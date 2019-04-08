66408664f9c7513b37cea8bd2bf13c86464ef660
user <user@ECHTP-51-16>
2019-04-08 19:33:40 +0300

    Add project files.

---
+++ AudioPlayer/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioPlayer
+{
+    class Song
+    {
+        int duration;
+        string title;
+        string path;
+        string lyrics;
+        string genre;
+        Artist Artist;
+        Album Album;
+        
+    }
+}
